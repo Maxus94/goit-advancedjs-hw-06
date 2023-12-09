565cf80995429798338b08aadccf82c83c5d1dbf
Maxim <kmaxusk@gmail.com>
2023-12-09 14:35:14 +0200

    correction

--- src/baseTypes/8.ts
+++ src/baseTypes/8.ts
@@ -3,9 +3,7 @@
   який може містити значення "male", "female". Створіть змінну myGender цього типу.
 */
 
-type Male = { male: boolean };
-type Female = { female: boolean };
-type Gender = Male | Female;
-const myGender: Gender = { male: true };
+type Gender = 'Male' | 'Female';
+const myGender: Gender = 'Male';
 
 export { };
